14d0e61d7e5fc8c5318192ebd2a7ba774b47b6fa
Guillaume POITTEVIN <gpo@atolcd.com>
2018-03-09 15:03:06 +0100

    Add error handling and better response

--- index.js
+++ index.js
@@ -5,49 +5,87 @@ const fs = require('fs');
 const resolve = require('path').resolve;
 const app = require('express')();
 
-const resultDir = '/result';
+const rootPath = process.env.ROOT_PATH || '';
 const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
-const resultBaseUrl = baseUrl + resultDir;
+const resultBaseUrl = baseUrl + '/result';
+
+const send = function (res, obj) {
+  res.status(obj.status);
+  for (let key in obj.headers) {
+    res.setHeader(key, obj.headers[key]);
+  }
+  res.send(JSON.stringify(obj.body));
+}
 
 var browser = null;
 
-fs.mkdirSync(resolve(resultDir));
+if (!fs.existsSync(resolve(rootPath + '/result'))) {
+  fs.mkdirSync(resolve(rootPath + '/result'));
+}
 
 app.listen(3000);
 
 app.route('/pdf/:url/:output')
   .get(async (req, res) => {
+    let result = {
+      status: 200,
+      body: {},
+      headers: {}
+    };
     const url = req.params.url;
     const filename = req.params.output;
 
-    if (browser === null) { // Is there a better way to get only one instance of browser ?
-      browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox'
-        ]
+    try {
+      if (browser === null) { // Is there a better way to get only one instance of browser ?
+        browser = await puppeteer.launch({
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox'
+          ]
+        });
+      }
+      const page = await browser.newPage();
+      //TODO: Do we need more params ? 
+      await page.emulateMedia('screen');
+      await page.goto(url);
+      await page.pdf({
+        path: resolve(rootPath + '/result/' + filename), format: 'A4'
       });
+
+      result.status = 200;
+      result.body = {
+        'url': resultBaseUrl + '/' + filename
+      };
+      result.headers = {
+        "Content-Type": "application/json"
+      };
+    } catch (e) {
+      result.status = 500;
+      result.body = {
+        'message': e.message
+      };
+      result.headers = {
+        "Content-Type": "application/json"
+      };
     }
-    const page = await browser.newPage();
 
-    // Do we need more params ? 
-    await page.emulateMedia('screen');
-    await page.goto(url);
-    await page.pdf({
-      path: resolve(resultDir + '/' + filename), format: 'A4'
-    });
-
-    //TODO: Add some error handling
-    res.send(JSON.stringify({
-      'url': resultBaseUrl + '/' + filename
-    }));
+    send(res, result);
   });
 
 //TODO: Is there a better way ?
-app.route(resultDir + '/:filename')
+app.route('/result/:filename')
   .get((req, res) => {
     const filename = req.params.filename;
 
-    res.sendFile(resolve(resultDir + '/' + filename));
+    res.sendFile(resolve(rootPath + '/result/' + filename));
+  })
+  .delete((req, res) => {
+    fs.unlinkSync(resolve(rootPath + '/result/' + filename));
+
+    send(res, {
+      status: 200,
+      body: { message: filename + ' was delete' },
+      headers: { "Content-Type": "application/json" }
+    });
   });
